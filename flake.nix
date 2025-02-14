bae7a2da97fdf7b05caf9bcc5584e0dc33de554c
FKouhai <frandres00@gmail.com>
2025-02-14 23:32:09 +0100

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   description = "urban-cli flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix = { 
+      url = "github:nix-community/gomod2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
-  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix } @inputs:
     (flake-utils.lib.eachDefaultSystem
       (system:
         let
