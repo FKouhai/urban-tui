355e2669fb3b7d22dcd89e7e168ba6fafa9d1799
FKouhai <frandres00@gmail.com>
2025-02-15 10:21:00 +0100

    updated flake

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self,nixpkgs }: {
     packages."x86_64-linux".default = with import nixpkgs { system = "x86_64-linux";};
-      stdenv.mkDerivation {
-        name = "urban-tui";
-        src = self;
-        buildInputs = [
+      buildGoModule rec {
+        pname = "urban-tui";
+        version = "0.1.0";
+        vendorHash = "sha256-93xVsTemE8HIvWKjBcArsyram1Z1WQhbF5yyig2yPLI=";
+        CGO_ENABLED = 0;
+        src = ./.;
+      nativeBuildInputs = [
           pkgs.go
-        ];
-        buildPhase = "go mod tidy && go build -o urban-tui";
-        installPhase = "mkdir -p $out/bin; install -t $out/bin urban-tui";
+          ];
+      postBuild = ''
+         go build -o urban-tui
+          '';
+      installPhase = ''
+         mkdir -p $out/bin
+         mv urban-tui $out/bin
+        '';
       };
+      
   };
 
 }
